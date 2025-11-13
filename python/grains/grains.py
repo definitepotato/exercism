557061359ff9bc116682107f86b05e65040cddc9
definitepotato <definitepotato@gmail.com>
2025-11-13 10:21:51 -0500

    task(python)[grains] submit exercise.

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,12 @@
 def square(number):
-    pass
+    if number == 1: return 1
+    if number == 0: raise ValueError("square must be between 1 and 64")
+    if number < 0 or number > 64: raise ValueError("square must be between 1 and 64")
+
+    n_grains = 1
+    for square in range(number-1): n_grains *= 2
+    return n_grains
 
 
 def total():
-    pass
+    return square(64) * 2 - 1
