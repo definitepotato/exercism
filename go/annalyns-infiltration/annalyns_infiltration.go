156e287292bbd83c8f77a79bdb1efe647432fd3b
definitepotato <definitepotato@gmail.com>
2025-11-12 19:15:35 -0500

    task(go)[annalyns-infiltration] cleaner syntax.

--- go/annalyns-infiltration/annalyns_infiltration.go
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -2,29 +2,17 @@ package annalyn
 
 // CanFastAttack can be executed only when the knight is sleeping.
 func CanFastAttack(knightIsAwake bool) bool {
-	if !knightIsAwake {
-		return true
-	}
-
-	return false
+	return !knightIsAwake
 }
 
 // CanSpy can be executed if at least one of the characters is awake.
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
-	if knightIsAwake || archerIsAwake || prisonerIsAwake {
-		return true
-	}
-
-	return false
+	return knightIsAwake || archerIsAwake || prisonerIsAwake
 }
 
 // CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping.
 func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
-	if prisonerIsAwake && !archerIsAwake {
-		return true
-	}
-
-	return false
+	return prisonerIsAwake && !archerIsAwake
 }
 
 // CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
