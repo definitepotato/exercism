b51375f5141c624e5268cf4c10ab651827a02448
definitepotato <definitepotato@gmail.com>
2025-11-24 15:45:02 -0500

    task(zig)[armstrong-numbers] declare where called.

--- zig/armstrong-numbers/armstrong_numbers.zig
+++ zig/armstrong-numbers/armstrong_numbers.zig
@@ -4,16 +4,15 @@ const pow = std.math.pow;
 pub fn isArmstrongNumber(num: u128) bool {
     if (num == 0) return true;
 
-    var sum: u128 = 0;
-    var my_num = num;
     var len_num: u128 = 0;
-
-    // get len of num
     var acc = num;
+    // get len of num
     while (acc > 0) : (acc /= 10) {
         len_num += 1;
     }
 
+    var sum: u128 = 0;
+    var my_num = num;
     while (my_num > 0) {
         sum += pow(u128, my_num % 10, len_num);
         my_num /= 10;
